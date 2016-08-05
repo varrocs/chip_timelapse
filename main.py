b5c123a9d84fbf22f89b402a9084de1e51b98539
varrocs <varrocs@gmail.com>
2016-08-05 21:16:55 +0200

    More data sent and more logic added to catch exceptions

--- main.py
+++ main.py
@@ -13,23 +13,26 @@ LOGFILE="timelapse.log"
 REBOOT_TIME=45
 FOR_REAL=False
 
-def log_data(switch_status):
+def log_data(switch_status, attiny_present):
     try:
-        if FOR_REAL:
+        try:
             power_supply.turn_on_current_adc_measurement()
             time.sleep(1)
             power_data = power_supply.read_power_data()
-        else:
+            power_data['powerdataStatus'] = True
+        except:
             power_data = {}
+            power_data['powerdataStatus'] = False
 
         log_data = power_data
         log_data['switchStatus'] = switch_status
         log_data['rebootTime'] = REBOOT_TIME
+        log_data['attinyPresent'] = attiny_present
 
         dweet_client.send_status_message(log_data)
-        logging.info("Power data: "+json.dumps(log_data))
+        logging.info("Status data: "+json.dumps(log_data))
     except Exception as e:
-        logging.error("[!] Failed to log power data" + str(e))
+        logging.error("[!] Failed to log status data" + str(e))
 
 def sync_data():
     logging.info(" ------------ Syncing files ...")
@@ -45,33 +48,33 @@ def power_off():
         print("poweroff_")
         subprocess.call("poweroff")
     else:
-        print("poweroff")
+        print("fake poweroff")
 
 def setup_reboot_timer():
     if FOR_REAL:
         reboot_timer.set_reboot_time(REBOOT_TIME)
 
 def is_stop_switch_active():
-    if FOR_REAL:
-        return reboot_timer.get_stop_switch_status()
-    else:
-        return False
-
+        try:
+            return reboot_timer.get_stop_switch_status(), True
+        except Exception as e:
+            return False, False
 
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG, filename=LOGFILE, filemode="a")
     logging.info(" ------------ Started ------------ ")
     take_photo()
-    switch_active = is_stop_switch_active()
-    log_data(switch_active)
+    switch_active, attiny_present = is_stop_switch_active()
+
+    log_data(switch_active, attiny_present)
     sync_data()
-    if not switch_active:
+    if not switch_active and attiny_present:
         setup_reboot_timer()
         logging.info(" ------------ Finished ------------ ")
         logging.shutdown()
         power_off()
     else:
-        logging.info("Stop switch is active! Quitting without poweroff")
+        logging.info("Stop switch is active or attiny is missing! Quitting without poweroff")
         sync_data()
 
 if __name__ == "__main__":
